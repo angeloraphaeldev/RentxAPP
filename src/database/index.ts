cac3f43e1c0a2ede2be54f8e05d27bc678ccfcc1
Raphael <raphaelfreire.dias@gmail.com>
2021-08-16 15:56:28 -0300

    feat: :sparkles: Adição de Container, e ajustes Docker

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,11 @@
 import { createConnection, getConnectionOptions } from "typeorm";
-
 interface IOptions {
   host: string;
 }
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
-  newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  newOptions.host = "database_ignite";
   createConnection({
     ...options,
   });
---
+++ src/shared/container/index.ts
@@ -0,0 +1,8 @@
+import { container } from "tsyringe";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoryRepositories";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoryRepository";
+//
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
