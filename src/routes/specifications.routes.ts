e931bb26e66c6fec3a46ddf596eb4c17da9f3c7a
Raphael <raphaelfreire.dias@gmail.com>
2021-08-15 23:13:19 -0300

    feat: :sparkles: Adição de Novas funçoes e refatoração de codigo

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+const specificationRoutes = Router();
+
+specificationRoutes.post("/", (req, res) => {
+  return createSpecificationController.handle(req, res);
+});
+
+specificationRoutes.get("/", (req, res) => {
+  return createSpecificationController.handle(req, res);
+});
+export { specificationRoutes };
