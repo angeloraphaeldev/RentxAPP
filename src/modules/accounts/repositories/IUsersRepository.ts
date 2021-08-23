3e44f3c679d462f7977e6a313aedc17541aad3c8
Raphael <raphaelfreire.dias@gmail.com>
2021-08-23 16:53:51 -0300

    feat: --

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { Users } from "../entities/User";
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<Users>;
 }
 
 export { IUsersRepository };
